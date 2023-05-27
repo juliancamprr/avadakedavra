e213fb70fb9e6664b937df174fae881ac16caaec
Djulian Campos <djuliancamposdc@gmail.com>
2023-05-26 21:28:55 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 const potter = document.getElementById('potter');
 const tiro = document.querySelector('.tiro');
 
-const showVar = () => {
 
-    tiro.style.display = 'block';
+
+const showVar = () => {
 
     const audio = new Audio('../sound/avada-kedavra-kurwa.mp3');
     audio.play();
+    tiro.style.display = 'block';
+
 
     setTimeout(() => {
         tiro.style.display = 'none';
